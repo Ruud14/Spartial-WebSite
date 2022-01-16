0014ff62bcac03dd33fa5ab66e86e637f23499ba
Ruud14 <ruudbrouwers@hotmail.com>
2022-01-16 16:34:47 +0100

    Created design

---
+++ assets/js/redirect.js
@@ -0,0 +1,4 @@
+// Redirect all shares to the home page.
+if (window.location.href.includes("/share/")) {
+  window.location.href = '/'; // go to index
+}
--- js.js
+++
@@ -1,7 +0,0 @@
-function openApp () {
-    var appWindow = window.open("spartial://");
-    setTimeout( function () {if (appWindow) {
-        appWindow.location ="https://google.com";
-            }
-            },1000);
-}
