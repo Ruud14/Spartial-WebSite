32814d61cc7d0e6635f8d4c77c43302b3e00f49f
Ruud14 <ruudbrouwers@hotmail.com>
2022-01-18 20:27:20 +0100

    Fix url to title image

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,6 +1,6 @@
 // Redirect all shares to the home page.
 if (window.location.href.includes("share")) {
-  window.location.href = 'https://spartial.app'; 
+    window.location.replace("https://spartial.app");
 }
 
 function onCopyFingerprint() {
