8cc42fb2ab8e89409cd497ef5213f80527c16c88
Ruud14 <ruudbrouwers@hotmail.com>
2022-01-18 20:11:50 +0100

    Made copying of setup content easier and fixed the redirect

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,4 +1,16 @@
 // Redirect all shares to the home page.
-if (window.location.href.includes("/share/")) {
-  window.location.href = '/'; // go to index
+if (window.location.href.includes("share")) {
+  window.location.href = 'https://spartial.app'; 
+}
+
+function onCopyFingerprint() {
+    navigator.clipboard.writeText("5C:A9:AD:18:3A:6E:B4:58:9D:75:86:EF:BA:F0:DE:16:F7:97:4C:57");
+}
+
+function onCopyRedirectUrl() {
+    navigator.clipboard.writeText("spartial://callback");
+}
+
+function onCopyPackageID() {
+    navigator.clipboard.writeText("com.ruud14.spartial");
 }
